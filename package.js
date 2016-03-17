703682bcfb057715b01a266b1826eee97ea505e2
Apratim Ankur <apratim.ankur@gmail.com>
2016-03-17 23:45:55 +0530

    package.js - jquery 1.11.3_2

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('jquery','client');
+  api.use('jquery@1.11.3_2','client');
   api.addFiles('roundslider.min.js','client');
   api.addFiles('roundslider.min.css','client');
 });
